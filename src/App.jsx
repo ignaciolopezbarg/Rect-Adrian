01993422f258dce4eef06ad59567d5bef0196ba5
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2025-03-03 16:03:41 -0300

    carga rc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-  }
-  return (
-  <div>
-   <h1>renderizado condicional</h1>
-  </div>
-  )
+  const condition = true;
 
+return (
+  <div>
+    <h1>renderizado condicional</h1>
+    {condition ? (
+      <h2>La condicion se cumple</h2>
+    ) : (
+      <h2>La condicion no se cumple</h2>
+    )}
+  </div>
+);
+}
 
 export default App;
