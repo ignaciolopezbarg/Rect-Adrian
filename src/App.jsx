4de3f12a9e42170016a69676f5ca8f8ee57b932c
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2025-03-03 16:13:10 -0300

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
 
 function App() {
-  const condition = true;
+  
 
 return (
   <div>
-    <h1>renderizado condicional</h1>
-    {condition ? (
-      <h2>La condicion se cumple</h2>
-    ) : (
-      <h2>La condicion no se cumple</h2>
-    )}
+    <h1>Renderizado de Listas</h1>
+    
+    
   </div>
 );
 }
