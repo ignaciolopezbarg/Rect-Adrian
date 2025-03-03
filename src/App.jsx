dba21721ea4978f5d8163309f51ba8eb179f2802
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2025-03-03 15:25:20 -0300

    cambios en cd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  
+  const [myText, setMyText] = useState('Tu nombre');
+  const [myValue, setMyValue] = useState('nachito');
 
+  const handleInput = (e) =>{
+    console.log(e.target.value);
+    setMyValue(e.target.value)
+  }
   return (
-  <div>Hola React</div>
+  <div>
+    <h1>Hola a todos</h1>
+    <h2>{myText}</h2>
+    <input type="text" 
+    placeholder={myText} 
+    value={myValue} 
+    onChange={handleInput}
+    />
+  </div>
   )
 }
 
-export default App
+export default App;
