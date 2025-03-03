ba8de258ee211b384df133fa40a4d2c2dd6077be
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2025-03-03 15:45:57 -0300

    plantilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 
-import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [myText, setMyText] = useState('Tu nombre');
-  const [myValue, setMyValue] = useState('nachito');
-
-  const handleInput = (e) =>{
-    console.log(e.target.value);
-    setMyValue(e.target.value)
+ 
   }
   return (
   <div>
-    <h1>Hola a todos</h1>
-    <h2>{myText}</h2>
-    <input type="text" 
-    placeholder={myText} 
-    value={myValue} 
-    onChange={handleInput}
-    />
+   <h1>renderizado condicional</h1>
   </div>
   )
-}
+
 
 export default App;
