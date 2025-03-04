f22cecda0b212879587db785e35f9f17822d03e9
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2025-03-04 08:31:46 -0300

    plantilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 
-function App() {
-  
+const movies = ["Lord of the Rings", "Star Wars", "Dune", "Despertares"];
 
-return (
-  <div>
-    <h1>Renderizado de Listas</h1>
-    
-    
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <h1>Renderizado de Listas</h1>
+      {movies.map((movie, index) => (
+        <p key={index}>
+          {index+1} - {movie}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
+
